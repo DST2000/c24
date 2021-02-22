a1bb2b0595524901dd436b7fc8330571a9822ecb
DST2000 <dnkst1c@rambler.ru>
2021-02-22 14:44:16 +0300

    c24 001 config

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-02-22 06:56:58 UTC
+#Software: Joomla! 4.0.0-beta8-dev Development [ Mañana ] 2-February-2021 20:02 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-02-22T06:56:58+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-02-22T06:57:03+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-02-22T06:57:18+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
