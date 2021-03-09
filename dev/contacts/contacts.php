949e45773849dd370661b5319f2c5d16914c052e
DST2000 <dnkst1c@rambler.ru>
2021-03-09 16:27:05 +0300

    c24 011

---
+++ dev/contacts/contacts.php
@@ -0,0 +1,11 @@
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d830.832802484882!2d27.583218105750298!3d53.9148637831393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcfa470d66791%3A0xfb386eae4d163b56!2z0L_Quy4g0K_QutGD0LHQsCDQmtC-0LvQsNGB0LA!5e0!3m2!1sru!2sby!4v1615295870393!5m2!1sru!2sby" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<p>Контакты</p>
+<p>Телефон:</p>
+<p>+375 33 355 45 50</p>
+<p>+375 44 455 45 50</p>
+
+<p>Адрес:</p>
+<p>РБ, г. Минск, м. Площадь Якуба Коласа</p>
+
+<p>Пн - Пт : 9:00 - 17:00</p>
+<p>Сб, Вс: выходной</p>
