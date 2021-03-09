7c572bafd61abccb73f48f3efd2e8ce1ede2502c
DST2000 <dnkst1c@rambler.ru>
2021-03-09 14:46:43 +0300

    c24 010

---
+++ dev/program1c/program1c.php
@@ -0,0 +1,275 @@
+<div class="sheme1c">
+	<span>Программы 1С:Предприятие</span>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="col-md-4 offset-md-1 clg">
+			Конфигурации
+		</div>
+		<div class="col-md-2 clg">
+			СУБД
+		</div>
+		<div class="col-md-4 clg">
+			Внешние обработки
+		</div>
+		<div class="col-md-1">
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="col-md-12 cl1c">
+			Платформа
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+</div>
+<div class="row g-0">
+</div>
+<div class="sheme1c">
+	<span>Платформы 1С: Предприятие</span>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="col-md-2 offset-md-1">
+			<span class="labelplatform">8.3</span>
+		</div>
+		<div class="col-md-2">
+			<span class="labelplatform">8.2</span>
+		</div>
+		<div class="col-md-2">
+			<span class="labelplatform">8.1</span>
+		</div>
+		<div class="col-md-2">
+			<span class="labelplatform">8.0</span>
+		</div>
+		<div class="col-md-2">
+			<span class="labelplatform">7.7</span>
+		</div>
+		<div class="col-md-1">
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+</div>
+<div class="row g-0">
+</div>
+<div class="sheme1c">
+	<span>Типовые конфигурации фирмы "1С" для Беларуси</span>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="config-header">
+			ERP Управление предприятием 2 для Беларуси
+		</div>
+		<div class="config-detail">
+			<div class="label-detail">
+				Номер актуальной версии:
+			</div>
+			<div class="number-label">
+				2.4.7.160
+			</div>
+			<div class="label-date">
+				от
+			</div>
+			<div class="config-date">
+				08.04.20
+			</div>
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="config-header">
+			Бухгалтерия для Беларуси, редакция 1.6
+		</div>
+		<div class="config-detail">
+			<div class="label-detail">
+				Номер актуальной версии:
+			</div>
+			<div class="number-label">
+				1.6.69.1
+			</div>
+			<div class="label-date">
+				от
+			</div>
+			<div class="config-date">
+				29.03.16
+			</div>
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="config-header">
+			Бухгалтерия для Беларуси, редакция 2
+		</div>
+		<div class="config-detail">
+			<div class="label-detail">
+				Номер актуальной версии:
+			</div>
+			<div class="number-label">
+				2.1.32.5
+			</div>
+			<div class="label-date">
+				от
+			</div>
+			<div class="config-date">
+				16.03.20
+			</div>
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="config-header">
+			Управление производственным предприятием для Беларуси
+		</div>
+		<div class="config-detail">
+			<div class="label-detail">
+				Номер актуальной версии:
+			</div>
+			<div class="number-label">
+				1.3.34.1
+			</div>
+			<div class="label-date">
+				от
+			</div>
+			<div class="config-date">
+				21.01.20
+			</div>
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="config-header">
+			Управление торговлей для Беларуси, редакция 2.0
+		</div>
+		<div class="config-detail">
+			<div class="label-detail">
+				Номер актуальной версии:
+			</div>
+			<div class="number-label">
+				2.0.48.1
+			</div>
+			<div class="label-date">
+				от
+			</div>
+			<div class="config-date">
+				10.10.17
+			</div>
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+</div>
+<div class="row g-0">
+</div>
+<div class="sheme1c">
+	<span>Персоны</span>
+	<div class="row g-0">
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Кашаев Сергей Михайлович
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Колиух Александр Викторович
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Гилев Евгений Михайлович
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Радченко Максим Григорьевич
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Хрусталева Елена Юрьевна
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Гладкий Алексей Анатольевич
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Селищев Николай Викторович
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Ажеронок В.А.
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+		<div class="col-md-5 book-author">
+			Габец Андрей Петрович
+		</div>
+		<div class="col-md-2 offset-md-2 books-list">
+			Книги
+		</div>
+		<div class="col-md-2 offset-md-1 about-author">
+			Об авторе
+		</div>
+	</div>
+	<div class="row g-0">
+	</div>
+</div>
