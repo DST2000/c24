418118fb81e8536c0e70598330f0323e30331687
DST2000 <dnkst1c@rambler.ru>
2021-04-12 14:48:09 +0300

    c24 012 services

---
+++ dev/services/services.php
@@ -0,0 +1,72 @@
+<div class="sheme1c">
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Обновление типовой конфигурации информационной базы</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+	<div class="row g-0">
+</div>
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Обновление конфигурации информационной базы с изменениями программного кода</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+	<div class="row g-0">
+</div>
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Загрузка (выгрузка) данных в (из) 1с в форматах xml, json, таблицы excel</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+	<div class="row g-0">
+</div>
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Консультации по вопросам ведения бухгалтерского и управленческого учёта в различных конфигурациях</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+	<div class="row g-0">
+</div>
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Организация "Бизнес процессов" для эффективной работы сотрудников с учетными системами</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+	<div class="row g-0">
+</div>
+	<div class="row g-0">
+		<div class="col-md-10">
+			<span class="labelservice">Создание и отладка существующих обменов данными программ на базе 1С:Предприятие с другими информационными системами</span>
+		</div>	
+		<div class="col-md-2">
+			<span class="labelcost">Калькулятор 
+стоимости
+ услуг</span>
+		</div>
+	</div>
+</div>
